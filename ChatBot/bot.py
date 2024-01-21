bb0ee26d645c4b824d603d56fd201ada92177bb7
HeniuM <gortag6@gmail.com>
2024-01-21 18:43:29 +0100

    Update of chatbot code and front

--- ChatBot/bot.py
+++ ChatBot/bot.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import openai
-import gradio as gr
-
 
 app = Flask(__name__)
+CORS(app)
 
 openai.api_key = ""
 
@@ -11,9 +11,16 @@ system_message = """You are customer support for
 problems with using the electric fireplace management application. You are helping
 customers with this chimney """
 
+custom_chatbot_message = """Hello. I'm your AI customer support. If you need help,
+ do not be afraid to talk with me!"""
 
-@app.route('/chat', methods=['Post'])
-def responseGPT(user_message, history):
+
+@app.route('/api/chatbot', methods=['POST'])
+def chatbot_endpoint():
+    data = request.json
+    user_message = data.get('user_message', '')
+    history = data.get('history', [])
+
     max_history_length = 16
     history_openai_format = []
 
@@ -38,18 +45,15 @@ def responseGPT(user_message, history):
     for chunk in response:
         if len(chunk['choices'][0]['delta']) != 0:
             partial_message = partial_message + chunk['choices'][0]['delta']['content']
-            yield partial_message
+
+    return jsonify({'response': partial_message, 'initial_message': custom_chatbot_message})
 
 
-custom_chatbot = gr.Chatbot([(None, "Hello. I'm your AI customer support. If you need help, do not be afraid to talk "
-                                    "with me!")], elem_id="chatbot", label="Chatbot")
+# Dodaj nowy endpoint dla początkowej wiadomości
+@app.route('/api/chatbot/initial-message', methods=['GET'])
+def get_initial_message():
+    return jsonify({'initial_message': custom_chatbot_message})
 
-with gr.Blocks() as demo:
-    # with gr.Row():
-    # gr.ChatInterface(fn=responseGPT)
-    gr.ChatInterface(
-        fn=responseGPT,
-        chatbot=custom_chatbot
-    )
 
-demo.queue().launch(share=True, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-import re
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fireplaces.db'
