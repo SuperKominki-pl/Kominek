d1d51522c89c643472af303a997c181db784fbb9
HeniuM <gortag6@gmail.com>
2024-01-26 16:46:17 +0100

    Update

--- MainFront/app/contact/page.js
+++ MainFront/app/contact/page.js
@@ -1,85 +1,102 @@
-import PlaceholderIcon from "../../public/assets/arrow-small-right.svg";
+"use client"
+import PlaceholderIcon from "../../public/assets/arrow-small-right.svg"
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
-import dynamic from 'next/dynamic'; // Import dynamic z next/dynamic
-
-const ClientOnlyComponent = dynamic(
-  () => import('../path-to-your-client-only-component'), // ścieżka do komponentu używającego useRef
-  { ssr: false } // Włącz wyłączanie Server-Side Rendering (SSR) dla tego komponentu
-);
-
+import {useEffect, useRef, useState} from "react";
+import axios from 'axios'
 export default function Page() {
+
+    const [messages, setMessages] = useState([]);
     const endOfMessagesRef = useRef(null);
     const [message, setMessage] = useState("");
 
-    // Przenieś logikę do komponentu po stronie klienta
-    const ClientComponent = () => {
-        const sendMessageToChatbot = async (userMessage) => {
-            try {
-                const response = await axios.post('http://51.68.155.42:5000/api/chatbot', {
-                    user_message: userMessage,
-                    history: message,
-                });
+    const receiveMessage = (text) => {
+        const newMessage = {id: messages.length + 1, text, sender: 'other'};
+        setMessages([...messages, newMessage]);
+    };
 
-                const { response: chatbotResponse, initial_message: customChatbotMessage } = response.data;
+    useEffect(() => {
+    const fetchInitialMessage = async () => {
+        try {
+            const response = await axios.get('http://51.68.155.42:5000/api/chatbot/initial-message');
+            const initialMessage = response.data.initial_message;
+            receiveMessage(initialMessage);
+        } catch (error) {
+            console.error('Error fetching initial message:', error);
+        }
+    };
 
-                // Aktualizacja stanu wiadomości w React
-                setMessage([...message, { text: chatbotResponse, sender: 'other' }]);
+    fetchInitialMessage();
+    endOfMessagesRef.current?.scrollIntoView({behavior: 'smooth'});
+}, []);
 
-                // Aktualizacja stanu wiadomości w React dla wiadomości początkowej
-                setMessage([...message, { text: customChatbotMessage, sender: 'other' }]);
-            } catch (error) {
-                console.error('Error sending message to chatbot:', error);
-            }
-        };
 
-        useEffect(() => {
-            endOfMessagesRef.current?.scrollIntoView({ behavior: 'smooth' });
-            // Dodaj wywołanie funkcji dla wiadomości początkowej po pierwszym renderowaniu komponentu
-            sendMessageToChatbot('');
-        }, []);
+    const sendMessage = async (text) => {
+    const newMessage = {id: messages.length + 1, text, sender: 'user'};
+    setMessages([...messages, newMessage]);
+    setMessage("");
 
-        return (
-            <section className={"px-[15px]"}>
-                <div className="chat-container">
-                    <div className="messages-container">
-                        {message.map((msg, index) => (
-                            <div key={index} className={`message ${msg.sender}`}>
-                                {msg.text}
-                            </div>
-                        ))}
-                        <div ref={endOfMessagesRef} />
-                    </div>
-                    <form
-                        className={"flex items-center rounded-md bg-[#292936] px-[16px] py-[12px]"}
+    try {
+        // Wyślij wiadomość do chatbota
+        const response = await axios.post('http://51.68.155.42:5000/api/chatbot', {
+            user_message: text,
+            history: messages,
+        });
+
+        // Odbierz odpowiedź od chatbota
+        const chatbotMessage = response.data.response;
+
+        // Dodaj odpowiedź chatbota do wiadomości
+        receiveMessage(chatbotMessage);
+    } catch (error) {
+        console.error('Error sending message to chatbot:', error);
+    }
+};
+
+
+    return (
+        <section className={"px-[15px]"}>
+
+            <div className="chat-container">
+                <div className="messages-container">
+                    {messages.map((message) => (
+                        <div key={message.id} className={`message ${message.sender}`}>
+                            {message.text}
+                        </div>
+                    ))}
+                    <div ref={endOfMessagesRef}/>
+                </div>
+                <form className={"flex items-center rounded-md bg-[#292936] px-[16px] py-[12px]"}
+                      onKeyPress={(e) => {
+                          if (e.key === 'Enter') {
+                              e.preventDefault();
+                          }
+                        }}
+                      >
+                    <input
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        type="text"
+                        className={"flex-grow bg-transparent outline-none text-white"}
+                        placeholder="Napisz wiadomość..."
                         onKeyPress={(e) => {
                             if (e.key === 'Enter') {
-                                e.preventDefault();
+                                sendMessage(e.target.value);
+                                e.target.value = '';
                             }
                         }}
-                    >
-                        <input
-                            value={message.text}
-                            onChange={(e) => setMessage({ ...message, text: e.target.value })}
-                            type="text"
-                            className={"flex-grow bg-transparent outline-none text-white"}
-                            placeholder="Napisz wiadomość..."
-                            onKeyPress={(e) => {
-                                if (e.key === 'Enter') {
-                                    sendMessageToChatbot(e.target.value);
-                                    e.target.value = '';
-                                }
-                            }}
-                        />
-                    </form>
+                    />
+                    {/*<span className={"p-3 bg-accentColor rounded-md"}*/}
+                    {/*    onClick={() => {*/}
+                    {/*        sendMessage(message);*/}
+                    {/*        setMessage('');*/}
+                    {/*    }}*/}
+                    {/*>*/}
+                    {/*    <Image src={PlaceholderIcon} alt={"placeholder-icon"} width={15} height={15}/>*/}
+                    {/*</span>*/}
+                </form>
 
-                    {/* Wyrenderuj komponent tylko po stronie klienta */}
-                    {typeof window !== 'undefined' && <ClientOnlyComponent />}
-                </div>
-            </section>
-        );
-    }
+            </div>
 
-    // Renderuj komponent po stronie klienta
-    return <ClientComponent />;
+        </section>
+    )
 }
